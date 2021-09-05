ddeab46d20d530ee687c17e0af4f5d8a5d92dff4
AlexCsmas <2alexcosmas@gmail.com>
2021-09-06 00:54:32 +0300

    Product counter working

--- components/Shared/Cart.js
+++ components/Shared/Cart.js
@@ -1,17 +1,21 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Toggle from "./Toggle";
-import OrderSummary from '~/components/Shared/OrderSummary'
+import OrderSummary from "~/components/Shared/OrderSummary";
+import { useMachine, useService } from "@xstate/react";
+import CartItem from "./CartItem";
 
-
-export default function CartComponent({ current }) {
+export default function CartComponent({ current, addItem }) {
   return (
     <>
       <div className="grid grid-cols-2 gap-10">
         <div>
           <h3 className="px-0 py-4 mt-4 text-xl font-bold">Basket</h3>
-
           {current.context.items.map((item, idx) => {
+            // console.log(item.ref.machine.context.name)
+            const [state, send] = useService(item.ref);
+            const itemCtx = state;
             return (
-              <div className="flex" key={idx}>
+              <div key={idx} className="flex my-5">
                 <div className="relative flex-none w-48">
                   <img
                     src="https://images.pexels.com/photos/1302883/pexels-photo-1302883.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
@@ -23,7 +27,7 @@ export default function CartComponent({ current }) {
                 <div className="flex-auto p-6 bg-white shadow-md">
                   <div className="flex flex-wrap">
                     <h1 className="flex-auto text-xl font-semibold">
-                      {item.name}
+                      {itemCtx.context.name}
                     </h1>
                     <div className="flex-none w-full my-1 text-sm font-medium text-gray-500">
                       Extra Details: With Extra Cream
@@ -35,13 +39,21 @@ export default function CartComponent({ current }) {
                         <button
                           className="flex items-center justify-center px-2 py-1 border border-gray-300 rounded-md"
                           type="button"
+                          onClick={() => {
+                            send("INC", { ...itemCtx })
+                            // itemCtx.context.quantity++
+                          }}
                         >
                           +
                         </button>
-                        <div>2</div>
+                        <div>{itemCtx.context.quantity}</div>
                         <button
                           className="flex items-center justify-center px-2 py-1 border border-gray-300 rounded-md"
                           type="button"
+                          onClick={() => {
+                            send("DEC", { ...itemCtx })
+                            // itemCtx.context.quantity++
+                          }}
                         >
                           -
                         </button>
@@ -49,58 +61,16 @@ export default function CartComponent({ current }) {
                     </div>
 
                     <div className="text-xl font-semibold text-gray-500">
-                      {item.amount}
+                    {current.context.currency} {itemCtx.context.amount}
                     </div>
                   </div>
                 </div>
               </div>
             );
           })}
-
-          <div className="flex my-10">
-            <div className="relative flex-none w-48">
-              <img
-                src="https://images.pexels.com/photos/1302883/pexels-photo-1302883.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                alt=""
-                className="absolute inset-0 object-cover w-full h-full"
-              />
-            </div>
-            <div className="flex-auto p-6 bg-white shadow-md">
-              <div className="flex flex-wrap">
-                <h1 className="flex-auto text-xl font-semibold">Red Velvet</h1>
-                <div className="flex-none w-full my-1 text-sm font-medium text-gray-500">
-                  Extra Details: With Extra Cream
-                </div>
-              </div>
-              <div className="flex justify-between mt-10">
-                <div className="inline-flex">
-                  <div className="flex items-center justify-between space-x-4">
-                    <button
-                      className="flex items-center justify-center px-2 py-1 border border-gray-300 rounded-md"
-                      type="button"
-                    >
-                      +
-                    </button>
-                    <div>2</div>
-                    <button
-                      className="flex items-center justify-center px-2 py-1 border border-gray-300 rounded-md"
-                      type="button"
-                    >
-                      -
-                    </button>
-                  </div>
-                </div>
-
-                <div className="text-xl font-semibold text-gray-500">
-                  $49.00
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-
         <div>
-          <OrderSummary />
+          <OrderSummary current={current} />
         </div>
       </div>
     </>
---
+++ components/Shared/CartItem.js
@@ -0,0 +1,46 @@
+export default function CartItem({ itemCtx, send }) {
+  return (
+    <div className="flex my-5">
+      <div className="relative flex-none w-48">
+        <img
+          src="https://images.pexels.com/photos/1302883/pexels-photo-1302883.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+          alt=""
+          className="absolute inset-0 object-cover w-full h-full"
+        />
+      </div>
+
+      <div className="flex-auto p-6 bg-white shadow-md">
+        <div className="flex flex-wrap">
+          <h1 className="flex-auto text-xl font-semibold">{itemCtx.context.name}</h1>
+          <div className="flex-none w-full my-1 text-sm font-medium text-gray-500">
+            Extra Details: With Extra Cream
+          </div>
+        </div>
+        <div className="flex justify-between mt-10">
+          <div className="inline-flex">
+            <div className="flex items-center justify-between space-x-4">
+              <button
+                className="flex items-center justify-center px-2 py-1 border border-gray-300 rounded-md"
+                type="button"
+                onClick={() => send("UPDATE", { ...itemCtx })}
+              >
+                +
+              </button>
+              <div>{itemCtx.context.quantity}</div>
+              <button
+                className="flex items-center justify-center px-2 py-1 border border-gray-300 rounded-md"
+                type="button"
+              >
+                -
+              </button>
+            </div>
+          </div>
+
+          <div className="text-xl font-semibold text-gray-500">
+            {itemCtx.context.amount}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
